a9d7aeac376aaf895737092c6b4260abe5589a33
Kaiyang Li <gamedevleo@gmail.com>
2020-12-16 23:08:18 +1100

    Amazon_clone header and home page

---
+++ src/components/Home.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import '../css/Home.css'
+
+export const Home = () => {
+
+  return (
+    <div className='home'>
+      <div className='home_container'>
+        <img className='home_image' src='https://images-fe.ssl-images-amazon.com/images/G/35/AU-hq/2020/img/Prime/XCM_Manual_1280301_1439664_AU_au_prime_gw_gesktop_hero_v1_1_3493098_1500x600_1X_en_AU._CB417954628_.jpg' alt='prime'></img>
+        <div className='home_row'>
+          {/* Product*/}
+          {/* Product*/}
+        </div>
+        <div className='home_row'>
+          {/* Product*/}
+          {/* Product*/}
+          {/* Product*/}
+        </div>
+        <div className='home_row'>
+          {/* Product*/}
+        </div>
+      </div>
+    </div>
+  )
+}
