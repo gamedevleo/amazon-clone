498da6559a54e6ef01b76ba1f108d4540754f1ee
Kaiyang Li <gamedevleo@gmail.com>
2020-12-28 15:56:32 +1100

    AmazonClone run build

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React,{useRef} from 'react';
 import '../css/Home.css';
 import {Product} from './Product';
-
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
 export const Home = () => {
+  const imgRef = useRef(null);
+  var imgArray = [];
+  imgArray[0] = 'https://images-fe.ssl-images-amazon.com/images/G/35/AU-hq/2020/img/Prime/XCM_Manual_1280301_1439664_AU_au_prime_gw_gesktop_hero_v1_1_3493098_1500x600_1X_en_AU._CB417954628_.jpg';
+  imgArray[1] ='https://i.gadgets360cdn.com/large/jio_fiber_amazon_prime_1591959546653.jpg';
+  imgArray[2] ='https://images-eu.ssl-images-amazon.com/images/G/02/prime/Gateway/new_ft_hero_van._CB430308860_.jpg';
+  imgArray[3] ='https://images-fe.ssl-images-amazon.com/images/G/35/x-site/events/BOXING-DAY/Hero/XCM_Manual_1297574_1519347_AU_AUSGBFCM_au_xsite_desktop_tall_hero_2x_3606723_1500x600_1X_en_AU._CB413514042_.jpg';
+  var i = 0;
+  const imageSlide=(e)=>{
+    const slideImage =document.getElementById('image');
+
+    if(e.target.id ==='rightButton'){
+      i++;
+      if(i>imgArray.length-1){
+        i=0;
+      }
+    }
+    if(e.target.id ==='leftButton'){
+      i--;
+      if(i<0){
+        i= imgArray.length-1;
+      }
+    }
+    slideImage.src = imgArray[i];
+  }
   return (
     <div className='home'>
       <div className='home_container'>
-        <img className='home_image' src='https://images-fe.ssl-images-amazon.com/images/G/35/AU-hq/2020/img/Prime/XCM_Manual_1280301_1439664_AU_au_prime_gw_gesktop_hero_v1_1_3493098_1500x600_1X_en_AU._CB417954628_.jpg' alt='prime'></img>
+        <div className="home_slide">
+          <NavigateBeforeIcon onClick={imageSlide} id='leftButton'className='home_slideLeftButton button'/>
+          <img id='image' ref={imgRef} className='home_image' src={imgArray[i]} alt='prime'></img>
+          <NavigateNextIcon onClick={imageSlide}id='rightButton'className='home_slideRightButton button'/>
+        </div>
         <div className='home_row'>
           <Product
             id={12131231231}
